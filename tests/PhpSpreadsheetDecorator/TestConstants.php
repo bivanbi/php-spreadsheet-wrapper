e31289b2c2274ecaf74b76ef6995849b391b3747
Balazs Lengyak <balazs@lengyak.hu>
2022-10-23 11:22:16 +0200

    Move test constants into a class of its own

---
+++ tests/PhpSpreadsheetDecorator/TestConstants.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace KignOrg\PhpSpreadsheetDecorator;
+
+class TestConstants
+{
+    const TEST_XLS_FILENAME = 'tests/testsheet.xlsx';
+    const COLUMN_HEADER_TEST_WORKSHEET_NAME = 'ColumnHeader';
+    const COLUMN_HEADER_WITH_INTERLEAVE_TEST_WORKSHEET_NAME = 'ColumnHeaderWithInterleave';
+
+    const HEADER_ROW = 1;
+    const HEADER_COLUMN = 'A';
+    const FIRST_DATA_ROW = 2;
+    const LAST_DATA_ROW = null;
+
+    const HEADER_COLUMNS = [
+        'A' => 'Name',
+        'B' => 'Age',
+        'C' => 'Home Country',
+        'D' => 'Occupation',
+    ];
+
+    const COLUMN_HEADER_TEST_SHEET_EXPECTED_CONTENT = [
+        2 => ['Name' => 'Barbara', 'Age' => 19, 'Home Country' => 'Germany', 'Occupation' => 'University Student'],
+        3 => ['Name' => 'Emmylou', 'Age' => 24, 'Home Country' => 'Netherlands', 'Occupation' => 'Writer'],
+        4 => ['Name' => 'Johnny', 'Age' => 65, 'Home Country' => 'USA', 'Occupation' => 'Musician'],
+        5 => ['Name' => 'Sammy', 'Age' => 34, 'Home Country' => 'Canada', 'Occupation' => 'Private Entrepreneur'],
+        6 => ['Name' => 'Walt', 'Age' => 102, 'Home Country' => 'Ireland', 'Occupation' => 'Retired'],
+    ];
+}
--- tests/PhpSpreadsheetDecorator/WorksheetDecorator/WorksheetWithColumnHeaderTest.php
+++ tests/PhpSpreadsheetDecorator/WorksheetDecorator/WorksheetWithColumnHeaderTest.php
@@ -5,34 +5,13 @@ namespace KignOrg\PhpSpreadsheetDecorator\WorksheetDecorator;
 use InvalidArgumentException;
 use KignOrg\PhpSpreadsheetDecorator\SpreadsheetDecorator;
 use KignOrg\PhpSpreadsheetDecorator\SpreadsheetLoader;
+use KignOrg\PhpSpreadsheetDecorator\TestConstants;
 use PhpOffice\PhpSpreadsheet\Exception as SpreadsheetException;
 use PhpOffice\PhpSpreadsheet\Reader\Exception as ReaderException;
 use PHPUnit\Framework\TestCase;
 
 class WorksheetWithColumnHeaderTest extends TestCase
 {
-    const XLS_FILENAME = 'tests/testsheet.xlsx';
-    const TEST_WORKSHEET_NAME = 'TestSheet';
-    const HEADER_ROW = 1;
-    const HEADER_COLUMN = "A";
-    const FIRST_DATA_ROW = 2;
-    const LAST_DATA_ROW = null;
-
-    const COLUMNS = [
-        "A" => "Name",
-        "B" => "Age",
-        "C" => "Home Country",
-        "D" => "Occupation",
-    ];
-
-    const EXPECTED_VALUES = [
-        2 => ["Name" => "Barbara", "Age" => 19, "Home Country" => "Germany", "Occupation" => "University Student"],
-        3 => ["Name" => "Emmylou", "Age" => 24, "Home Country" => "Netherlands", "Occupation" => "Writer"],
-        4 => ["Name" => "Johnny", "Age" => 65, "Home Country" => "USA", "Occupation" => "Musician"],
-        5 => ["Name" => "Sammy", "Age" => 34, "Home Country" => "Canada", "Occupation" => "Private Entrepreneur"],
-        6 => ["Name" => "Walt", "Age" => 102, "Home Country" => "Ireland", "Occupation" => "Retired"],
-    ];
-
     protected SpreadsheetDecorator $spreadsheet;
     protected WorksheetWithColumnHeader $worksheet;
 
@@ -41,10 +20,10 @@ class WorksheetWithColumnHeaderTest extends TestCase
      */
     protected function setUp(): void
     {
-        $this->spreadsheet = SpreadsheetLoader::loadFromFile(self::XLS_FILENAME);
-        $this->worksheet = $this->spreadsheet->getWorksheetWithColumnHeader(self::TEST_WORKSHEET_NAME);
-        $this->worksheet->setHeaderRow(self::HEADER_ROW);
-        $this->worksheet->setFirstDataRow(self::FIRST_DATA_ROW);
+        $this->spreadsheet = SpreadsheetLoader::loadFromFile(TestConstants::TEST_XLS_FILENAME);
+        $this->worksheet = $this->spreadsheet->getWorksheetWithColumnHeader(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
+        $this->worksheet->setHeaderRow(TestConstants::HEADER_ROW);
+        $this->worksheet->setFirstDataRow(TestConstants::FIRST_DATA_ROW);
     }
 
     public function testGetColumnMap_withHeaderRowBeyondHighest()
@@ -57,14 +36,14 @@ class WorksheetWithColumnHeaderTest extends TestCase
     public function testGetColumnMap_withValidHeaderRow()
     {
         $actual = $this->worksheet->getColumnMap();
-        $this->assertEquals(array_flip(self::COLUMNS), $actual);
+        $this->assertEquals(array_flip(TestConstants::HEADER_COLUMNS), $actual);
     }
 
     public function testRowIterator()
     {
         $rowIterator = $this->worksheet->getRowIterator();
         foreach ($rowIterator as $row) {
-            $expected = self::EXPECTED_VALUES[$row->getRowIndex()];
+            $expected = TestConstants::COLUMN_HEADER_TEST_SHEET_EXPECTED_CONTENT[$row->getRowIndex()];
             $actual = $row->asArray();
             self::assertEquals($expected, $actual);
         }
@@ -84,14 +63,14 @@ class WorksheetWithColumnHeaderTest extends TestCase
 
     public function testGetColumnIndex_withValidName()
     {
-        foreach (self::COLUMNS as $columnIndex => $columnName) {
+        foreach (TestConstants::HEADER_COLUMNS as $columnIndex => $columnName) {
             self::assertEquals($columnIndex, $this->worksheet->getColumnIndex($columnName));
         }
     }
 
     public function testGetCellByColumnNameAndRow()
     {
-        foreach (self::EXPECTED_VALUES as $rowIndex => $columns) {
+        foreach (TestConstants::COLUMN_HEADER_TEST_SHEET_EXPECTED_CONTENT as $rowIndex => $columns) {
             foreach ($columns as $columnName => $expectedValue) {
                 $cell = $this->worksheet->getCellByColumnNameAndRow($columnName, $rowIndex);
                 self::assertEquals($expectedValue, $cell->getValue());
