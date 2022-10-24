09ad692bcd3070b4f8211e61734ea29b487febbb
Balazs Lengyak <balazs@lengyak.hu>
2022-10-24 19:11:57 +0200

    Make $worksheet variable local
    As we are using multiple different worksheets throughout
    the tests, it makes sense to make it a local variable.

--- tests/PhpSpreadsheetDecorator/WorksheetDecorator/WorksheetWithColumnHeaderTest.php
+++ tests/PhpSpreadsheetDecorator/WorksheetDecorator/WorksheetWithColumnHeaderTest.php
@@ -13,7 +13,6 @@ use PHPUnit\Framework\TestCase;
 class WorksheetWithColumnHeaderTest extends TestCase
 {
     protected SpreadsheetDecorator $spreadsheet;
-    protected WorksheetWithColumnHeader $worksheet;
 
     /**
      * @throws ReaderException
@@ -21,34 +20,34 @@ class WorksheetWithColumnHeaderTest extends TestCase
     protected function setUp(): void
     {
         $this->spreadsheet = SpreadsheetLoader::loadFromFile(TestConstants::TEST_XLS_FILENAME);
-        $this->worksheet = $this->spreadsheet->getWorksheetWithColumnHeader(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
-        $this->worksheet->setHeaderRow(TestConstants::HEADER_ROW);
-        $this->worksheet->setFirstDataRow(TestConstants::FIRST_DATA_ROW);
     }
 
     public function testGetColumnMap_withHeaderRowBeyondHighest()
     {
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
         $this->expectException(SpreadsheetException::class);
-        $this->worksheet->setHeaderRow(10000);
-        $this->worksheet->getColumnMap();
+        $worksheet->setHeaderRow(10000);
+        $worksheet->getColumnMap();
     }
 
     public function testGetColumnMap_withValidHeaderRow()
     {
-        $actual = $this->worksheet->getColumnMap();
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
+        $actual = $worksheet->getColumnMap();
         $this->assertEquals(array_flip(TestConstants::HEADER_COLUMNS), $actual);
     }
 
     public function testGetColumnMap_withInterleavedHeader()
     {
-        $worksheet = $this->getColumnHeaderWorksheetWithInterleave();
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_WITH_INTERLEAVE_TEST_WORKSHEET_NAME);
         $actual = $worksheet->getColumnMap();
         $this->assertEquals(array_flip(TestConstants::HEADER_COLUMNS_WITH_INTERLEAVE), $actual);
     }
 
     public function testRowIterator()
     {
-        $rowIterator = $this->worksheet->getRowIterator();
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
+        $rowIterator = $worksheet->getRowIterator();
         foreach ($rowIterator as $row) {
             $expected = TestConstants::COLUMN_HEADER_TEST_SHEET_EXPECTED_CONTENT[$row->getRowIndex()];
             $actual = $row->asArray();
@@ -59,27 +58,31 @@ class WorksheetWithColumnHeaderTest extends TestCase
     public function testGetColumnNameByAddress_withInvalidAddress()
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->worksheet->getColumnNameByAddress('Z');
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
+        $worksheet->getColumnNameByAddress('Z');
     }
 
     public function testGetColumnAddressByName_withInvalidName()
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->worksheet->getColumnAddressByName("NON-existing-column-name");
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
+        $worksheet->getColumnAddressByName("NON-existing-column-name");
     }
 
     public function testGetColumnAddressByName_withValidName()
     {
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
         foreach (TestConstants::HEADER_COLUMNS as $ColumnAddress => $columnName) {
-            self::assertEquals($ColumnAddress, $this->worksheet->getColumnAddressByName($columnName));
+            self::assertEquals($ColumnAddress, $worksheet->getColumnAddressByName($columnName));
         }
     }
 
     public function testGetCellByColumnNameAndRow()
     {
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
         foreach (TestConstants::COLUMN_HEADER_TEST_SHEET_EXPECTED_CONTENT as $rowIndex => $columns) {
             foreach ($columns as $columnName => $expectedValue) {
-                $cell = $this->worksheet->getCellByColumnNameAndRow($columnName, $rowIndex);
+                $cell = $worksheet->getCellByColumnNameAndRow($columnName, $rowIndex);
                 self::assertEquals($expectedValue, $cell->getValue());
             }
         }
@@ -87,24 +90,25 @@ class WorksheetWithColumnHeaderTest extends TestCase
 
     public function testAddColumn_viaAddColumnMethod()
     {
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_TEST_WORKSHEET_NAME);
         $expectedColumnMap = array_merge(TestConstants::HEADER_COLUMNS, ['E' => 'New Column']);
-        $this->worksheet->addColumn($expectedColumnMap['E']);
-        $actualColumnMap = $this->worksheet->getColumnMap();
+        $worksheet->addColumn($expectedColumnMap['E']);
+        $actualColumnMap = $worksheet->getColumnMap();
         self::assertEquals(array_flip($expectedColumnMap), $actualColumnMap);
     }
 
     public function testAddColumn_viaAddColumnMethod_withInterleavedHeader()
     {
-        $worksheet = $this->getColumnHeaderWorksheetWithInterleave();
+        $worksheet = $this->getWorksheet(TestConstants::COLUMN_HEADER_WITH_INTERLEAVE_TEST_WORKSHEET_NAME);
         $expectedColumnMap = array_merge(TestConstants::HEADER_COLUMNS, ['B' => 'New Column']);
         $worksheet->addColumn($expectedColumnMap['B']);
         $actualColumnMap = $worksheet->getColumnMap();
         self::assertEquals(array_flip($expectedColumnMap), $actualColumnMap);
     }
 
-    private function getColumnHeaderWorksheetWithInterleave(): WorksheetWithColumnHeader
+    private function getWorksheet(string $worksheetName): WorksheetWithColumnHeader
     {
-        $worksheet = $this->spreadsheet->getWorksheetWithColumnHeader(TestConstants::COLUMN_HEADER_WITH_INTERLEAVE_TEST_WORKSHEET_NAME);
+        $worksheet = $this->spreadsheet->getWorksheetWithColumnHeader($worksheetName);
         $worksheet->setHeaderRow(TestConstants::HEADER_ROW);
         $worksheet->setFirstDataRow(TestConstants::FIRST_DATA_ROW);
         return $worksheet;
